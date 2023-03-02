6c403b0f69bbb33a631907e9a1e8fd2be476d3c5
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-03-02 01:14:39 -0800

    genesis

---
+++ README.md
@@ -0,0 +1,1 @@
+repo that holds all the problems i have worked on codeforces.com
---
+++ src/watermelon.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+  int number = 0;
+ 
+  cin >> number;
+  
+  if (number == 2) {
+    cout << "NO" << endl;
+  }
+  else if (number % 2 == 0) {
+    cout << "YES" << endl;
+  }
+  else {
+    cout << "NO" << endl;
+  }
+ 
+  return 0;
+}
