fa1f1df42ba4135bfa92f6dd63b1a651dc8a8928
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-03-02 01:22:31 -0800

    problem 282a

---
+++ src/bit.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int lines = 0;
+
+  cin >> lines;
+
+  int total = 0;
+  string temp;
+
+  for (int i = 0; i < lines; i++) {
+    cin >> temp;
+
+    if (temp.at(0) == + || temp.at(2) == +) {
+      total++;
+    } else if (temp.at(0) == - || temp.at(2) == -) {
+      total--;
+    }
+  }
+
+  cout << total << endl;
+
+  return 0;
+}
