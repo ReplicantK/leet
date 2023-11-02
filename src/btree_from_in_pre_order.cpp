0259e93eec6aebb1339d605145c872be59da221b
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-11-02 07:16:59 -0700

    previous commit, optimize with hashmap

--- src/btree_from_in_pre_order.cpp
+++ src/btree_from_in_pre_order.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -12,23 +13,16 @@ struct TreeNode {
   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder, int preorder_start, int inorder_start, int inorder_end) {
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder, int preorder_start, int inorder_start, int inorder_end, unordered_map<int, int>& mp) {
   if (preorder_start > preorder.size() - 1 || inorder_start > inorder_end) {
     return nullptr;
   }
 
   TreeNode* parent = new TreeNode(preorder[preorder_start]);
+  int inorder_index = mp[parent->val];
 
-  int inorder_index = 0;
-  for (int i = inorder_start; i <= inorder_end; i++) {
-    if (inorder[i] == parent->val) {
-      inorder_index = i;
-      break;
-    }
-  }
-
-  parent->left = buildTree(preorder, inorder, preorder_start + 1, inorder_start, inorder_index - 1);
-  parent->right = buildTree(preorder, inorder, preorder_start + inorder_index - inorder_start + 1, inorder_index + 1, inorder_end);
+  parent->left = buildTree(preorder, inorder, preorder_start + 1, inorder_start, inorder_index - 1, mp);
+  parent->right = buildTree(preorder, inorder, preorder_start + inorder_index - inorder_start + 1, inorder_index + 1, inorder_end, mp);
 
   return parent;
 }
@@ -37,7 +31,13 @@ int main() {
   vector<int> preorder = {3,9,20,15,7};
   vector<int> inorder = {9,3,15,20,7}; 
 
-  auto test = buildTree(preorder, inorder, 0, 0, inorder.size() - 1);
+  unordered_map<int, int> mp;
+
+  for (int i = 0; i < inorder.size(); i++) {
+    mp[inorder[i]] = i;
+  }
+
+  auto test = buildTree(preorder, inorder, 0, 0, inorder.size() - 1, mp);
 
   cout << test->val << endl;
   cout << test->left->val << endl;
