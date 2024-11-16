b429981f3cdee4955e555c8f5c0af5ad052bf703
sevenwhiteclouds <maldonadokeldin@gmail.com>
2024-11-16 01:58:57 -0800

    resolve leetcode invert binary tree

--- leetcode/invert_b_tree.cc
+++ leetcode/invert_b_tree.cc
@@ -1,6 +1,6 @@
 #include <iostream>
-
-using namespace std;
+#include <queue>
+#include <stack>
 
 struct TreeNode {
   int val;
@@ -12,26 +12,89 @@ struct TreeNode {
   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-void invert(TreeNode* node) {
-  if (node == nullptr) {
-    return; 
-  }
-  
-  invert(node->left);
-  invert(node->right);
+TreeNode* invertTree(TreeNode* root) {
+  std::stack<TreeNode*> st;
 
-  TreeNode* temp_left = node->left;
-  node->left = node->right;
-  node->right = temp_left;
+  if (root) {
+    st.push(root);
+  }
+
+  while (!st.empty()) {
+    TreeNode* current = st.top();
+    st.pop();
+
+    TreeNode* swap = current->left;
+    current->left = current->right;
+    current->right = swap;
+
+    if (current->left) {
+      st.push(current->left);
+    }
+
+    if (current->right) {
+      st.push(current->right);
+    }
+  }
+
+  return root;
+}
+
+void treePrinterAndDelete(TreeNode* root) {
+  if (!root) {
+    return;
+  }
+
+  std::queue<TreeNode*> q;
+  std::vector<int> v;
+
+  q.push(root);
+
+  while (!q.empty()) {
+    TreeNode* current = q.front();
+    q.pop();
+
+    v.push_back(current->val);
+
+    if (current->left) {
+      q.push(current->left);
+    }
+
+    if (current->right) {
+      q.push(current->right);
+    }
+
+    delete(current);
+  }
+
+  for (int i = 0; i < v.size() - 1; i++) {
+    std::cout << v[i] << ",";
+  }
+
+  std::cout << v[v.size() - 1];
 }
 
 int main() {
-  // test case
-  TreeNode* root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
-  root->left->left = new TreeNode(5);
-  root->left->right = new TreeNode(6);
+  // case 1
+  TreeNode* root = new TreeNode(
+    4, new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+    new TreeNode(7, new TreeNode(6), new TreeNode(9))
+  );
 
-  invert(root);
+  std::cout << "CASE1\nExpected: [4,7,2,9,6,3,1]\nReceived: [";
+  treePrinterAndDelete(invertTree(root));
+
+  // case 2
+  root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+
+  std::cout << "]\n\nCASE2\nExpected: [2,3,1]\nReceived: [";
+  treePrinterAndDelete(invertTree(root));
+
+  // case 3
+  root = nullptr;
+
+  std::cout << "]\n\nCASE3\nExpected: []\nReceived: [";
+  treePrinterAndDelete(invertTree(root));
+  std::cout << "]" << std::endl;
   
   return 0;
 }
