f6573f52299999082b0164bece0bcdfca77a1df1
ReplicantK <maldonadokeldin@gmail.com>
2024-11-19 06:28:58 -0800

    resolve leetcode diameter of binary tree

--- leetcode/diameter_of_btree.cc
+++ leetcode/diameter_of_btree.cc
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <utility>
-
-using namespace std;
 
 struct TreeNode {
   int val;
@@ -12,76 +9,73 @@ struct TreeNode {
   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-pair<int, int> edges(TreeNode* node) {
-  if (node == nullptr) {
-    return {-69, -1};
-  }
-  
-  auto left = edges(node->left);
-  auto right = edges(node->right);
-
-  left.second += 1;
-  right.second += 1;
-
-  int it_total = left.second + right.second;
-  int current_max = left.first;
-
-  if (right.first > current_max) {
-    current_max = right.first;
-  }
-  if (it_total > current_max) {
-    current_max = it_total;
+int recurse(TreeNode* root, int& d) {
+  if (!root) {
+    return 0;
   }
 
-  int it_win = 0;
-  if (left.second >= right.second) {
-    it_win = left.second;
-  } else {
-    it_win = right.second;
+  int left = recurse(root->left, d);
+  int right = recurse(root->right, d);
+
+  if (left + right > d) {
+    d = left + right;
   }
 
-  return {current_max, it_win};
+  if (left > right) {
+    return left + 1;
+  }
+
+  return right + 1;
+}
+
+int diameterOfBinaryTree(TreeNode* root) {
+  int d = 0;
+  recurse(root, d);
+  return d;
+}
+
+void deleteNodes(TreeNode* root) {
+  if (!root) {
+    return;
+  }
+
+  deleteNodes(root->left);
+  deleteNodes(root->right);
+
+  delete(root);
+  return;
 }
 
 int main() {
   // test case 1
-  //TreeNode* root = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+  TreeNode* root = new TreeNode(
+    1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3)
+  );
+
+  int d = diameterOfBinaryTree(root);
+  std::cout << "CASE 1\nExpected: 3\nReceived: " << d << "\n\n";
+  deleteNodes(root);
 
   // test case 2
-  //TreeNode* root = new TreeNode(1);
-  //root->left = new TreeNode(2);
+  root = new TreeNode(1, new TreeNode(2), nullptr);
 
+  d = diameterOfBinaryTree(root);
+  std::cout << "CASE 2\nExpected: 1\nReceived: " << d << "\n\n";
+  deleteNodes(root);
+  
   // test case 3
-  //TreeNode* root = new TreeNode(4);
-  //root->left = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+  root = new TreeNode(4, new TreeNode(-7), new TreeNode(-3,
+    new TreeNode(-9, new TreeNode(9, new TreeNode(6,
+    new TreeNode(0, nullptr, new TreeNode(-1)), new TreeNode(6,
+    new TreeNode(-4), nullptr)), nullptr), new TreeNode(-7,
+    new TreeNode(-6, new TreeNode(5), nullptr), new TreeNode(-6,
+    new TreeNode(9, new TreeNode(-2), nullptr), nullptr))),
+    new TreeNode(-3, new TreeNode(-4), nullptr))
+  );
 
-  // test case 4
-  TreeNode* root = new TreeNode(1, new TreeNode(5), new TreeNode(7));
-  root->left->left = new TreeNode(9);
-  root->left->left->left = new TreeNode(3);
-  root->left->left->left->right = new TreeNode(4);
-  root->left->right = new TreeNode(2);
-  root->left->right->left = new TreeNode(15);
-  root->left->right->right = new TreeNode(69);
-  root->left->right->right->right = new TreeNode(2040);
-
-  auto left = edges(root->left);
-  auto right = edges(root->right);
-
-  int sub_max = 0;
-  if (left.first >= right.first) {
-    sub_max = left.first;
-  } else {
-    sub_max = right.first;
-  }
-
-  int root_max = (left.second + 1) + (right.second + 1);
-
-  if (sub_max >= root_max) {
-    cout << sub_max << endl;
-  } else {
-    cout << root_max << endl;
-  }
+  d = diameterOfBinaryTree(root);
+  std::cout << "CASE 3\nExpected: 8\nReceived: " << d << std::endl;
+  deleteNodes(root);
 
   return 0;
 }
